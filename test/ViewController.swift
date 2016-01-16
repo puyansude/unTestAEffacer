f3c05068770b9edd6e506e71afad67ac9ca49495
Alain <alain@alains-mac.local>
2016-01-16 10:37:46 -0500

    juste avant le merge

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -1,25 +1,73 @@
-//
 //  ViewController.swift
-//  test
-//
-//  Created by Alain on 2016-01-15.
-//  Copyright © 2016 Alain. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        var j = 0
+        let chaine = "bonjour le monde"
+        for i in 1...10 {
+            j = i % 4
+            print("\(i) - \(chaine)!!".uppercaseString.lowercaseString.capitalizedString)
+            if j == 0 {
+               print("\(i) est un multiple de 4")
+            }
+        }  // for i
+        
+        // http://stackoverflow.com/questions/30767594/iterate-through-a-string-swift-2-0
+        for car in chaine.unicodeScalars {
+             print("\(car.value):\(car) ", terminator: "")
+        
+        }
+        for car in chaine.characters {
+            print("\(car) ", terminator: "")
+        }
+        for car in chaine.characters.reverse() {
+            print("\(car) ", terminator: "")
+        }
+        print("")
+        //
+        
+        testerIntrospection()
+        
+    } // viewDidLoad()
 
+    // MARK: - Mes fonctions
+    
+    func testerIntrospection(){
+    
+        class Maitre {
+            var type:String { get {return "Maitre"} }
+        }
+        
+        class Esclave: Maitre {
+            override var type:String { get {return "Maitre"} }
+        }
+        
+        let personnage: Any = Esclave()
+        
+        if (personnage is Maitre) {
+            print("est de type \( (personnage as! Maitre).type)")
+        }
+        
+        if (personnage is Esclave) {
+            print("est de type Esclave")
+        }
+        
+    }
+    
+    // MARK: - Méthodes de protocoles
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print(textField.text!)
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print("Oups, manque de mémoire ...")
     }
-
-
-}
+} // class ViewController
 
